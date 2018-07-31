1f2a4d9f585bcef5d7edf124d6dd2ed41eea7299
EricRybarczyk <EricRybarczyk@gmail.com>
2018-07-31 19:06:43 -0400

    Added simple delay animation for display of punchline

--- JokeActivity/src/main/java/com/example/jokeactivity/DisplayJokeActivity.java
+++ JokeActivity/src/main/java/com/example/jokeactivity/DisplayJokeActivity.java
@@ -3,6 +3,7 @@ package com.example.jokeactivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
 import android.widget.TextView;
 
 public class DisplayJokeActivity extends AppCompatActivity {
@@ -20,7 +21,16 @@ public class DisplayJokeActivity extends AppCompatActivity {
         TextView setupText = findViewById(R.id.joke_setup_text);
         TextView punchlineText = findViewById(R.id.joke_punchline_text);
 
+        // Fade-in animation for joke punchline. Longer offset for longer joke setups, minimum 2 seconds
+        AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        int wordCount = jokeSetup.split("\\s+").length;
+        int offset = 200 * (wordCount < 10 ? 10 : wordCount);
+        fadeIn.setDuration(1500);
+        fadeIn.setStartOffset(offset);
+
         setupText.setText(jokeSetup);
         punchlineText.setText(jokePunchline);
+
+        punchlineText.startAnimation(fadeIn);
     }
 }
